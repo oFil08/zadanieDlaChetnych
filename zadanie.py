851b3e09e488a5a58bf412666c11291e64a1b50e
Filakiewicz.jr <oliwierfilak08@gmail.com>
2025-01-14 08:43:38 +0100

    Add files via upload

--- zadanie.py
+++ zadanie.py
@@ -1,11 +1,21 @@
-plik = open("napisy.txt", "r")
+plik = open("przyklad.txt", "r")
 
-#4.1
+# 4.1
 w = 0
 for linijka in plik:
     for znak in linijka:
-        if ord(znak) >= 48 and ord(znak) <= 58:
-           w = w+1
+        if ord(znak) >= 48 and ord(znak) <= 57:
+            w += 1
 print(w)
 
-#4.2
+plik.seek(0)
+
+# 4.2
+odpowiedz = []
+for i in range(50):
+    plik.seek(((i + 1)*20)*50)
+    line = plik.readline()
+    odpowiedz.append(line)
+print("".join(odpowiedz))
+
+plik.close()
