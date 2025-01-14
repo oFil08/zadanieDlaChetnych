926898f99b4b589a55dad2a4f7f901ec93be7b0d
Filakiewicz.jr <oliwierfilak08@gmail.com>
2025-01-15 00:31:49 +0100

    Add files via upload

--- zadanie.py
+++ zadanie.py
@@ -1,21 +1,56 @@
-plik = open("przyklad.txt", "r")
+def isPali(s):
+    return s == s[::-1]
 
 # 4.1
-w = 0
-for linijka in plik:
-    for znak in linijka:
-        if ord(znak) >= 48 and ord(znak) <= 57:
-            w += 1
-print(w)
-
-plik.seek(0)
+with open("napisy.txt", "r") as file:
+    w = 0
+    for line in file:
+        w += sum(1 for char in line if '0' <= char <= '9')
+    print(w)
 
 # 4.2
-odpowiedz = []
-for i in range(50):
-    plik.seek(((i + 1)*20)*50)
-    line = plik.readline()
-    odpowiedz.append(line)
-print("".join(odpowiedz))
+with open("napisy.txt", "r") as file:
+    answ1 = []
+    i1 = 0
+    i2 = 0
+    for line in file:
+        line = line.strip()
+        i1 += 1
+        if i1 % 20 == 0:
+            if i2 < len(line):
+                answ1.append(line[i2])
+                i2 += 1
+    print("".join(answ1))
 
-plik.close()
+# 4.3
+with open("napisy.txt", "r") as file:
+    answ2 = []
+    for line in file:
+        line = line.strip()
+        if isPali(line + line[0]):
+            answ2.append(line[25])
+        elif isPali(line[49] + line):
+            answ2.append(line[24])
+    print("".join(answ2))
+
+# 4.4
+with open("napisy.txt", "r") as file:
+    result = []
+    stop_counter = 0
+
+    for line in file:
+        digits = "".join(char for char in line if '0' <= char <= '9')
+        digits = digits[:len(digits) - len(digits) % 2]
+
+        for i in range(0, len(digits), 2):
+            num = int(digits[i:i + 2])
+            if 65 <= num <= 90:
+                char = chr(num)
+                result.append(char)
+                stop_counter = stop_counter + 1 if char == 'X' else 0
+                if stop_counter == 3:
+                    print("".join(result))
+                    exit()
+
+    print("".join(result))
+
